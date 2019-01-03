a4ef42a857717f61707c3e4a843646a47e8b5409
mercury <udtrokia@gmail.com>
2019-01-03 12:33:04 +0800

    panel.css

--- src/views/panel.js
+++ src/views/panel.js
@@ -7,6 +7,7 @@
 //#home.js
 import {DOM, Element} from '../lib';
 import '../styles/panel.css';
+import '../styles/pics.css';
 
 /* const */
 const e = new Element();
@@ -28,15 +29,16 @@ let safe_panel = e.i('div', {'class': 'safe safe_panel'}).t(page_panel);
 //@app-wrapper
 let wrapper_app_panel = e.i('div', {'class': 'wrapper_app_panel'}).t(safe_panel);
 let wrapper_app_panel_safe = e.i('div', {'class': 'wrapper_app_panel_safe'}).t(wrapper_app_panel);
-e.i('div', {'class': 'app'}).t(wrapper_app_panel_safe);
-e.i('div', {'class': 'app'}).t(wrapper_app_panel_safe);
-e.i('div', {'class': 'app'}).t(wrapper_app_panel_safe);
-e.i('div', {'class': 'app'}).t(wrapper_app_panel_safe);
-e.i('div', {'class': 'app'}).t(wrapper_app_panel_safe);
-e.i('div', {'class': 'app'}).t(wrapper_app_panel_safe);
+let apps = ['space', 'groups', 'store', 'work', 'wallet', 'investment'];
+for (let i in apps) {
+  let app = e.i('div', {'class': `app ${apps[i]}`}).t(wrapper_app_panel_safe);
+  e.i('text', {'class': 'text_app'}, {}, `${apps[i]}`).t(app);
+}
 
 //@tab-bar
 let tab_bar = e.i('div', {'class': 'tab_bar'}).t(page_panel);
 let tab_bar_safe = e.i('div', {'class': 'tab_bar_safe'}).t(tab_bar);
-e.i('div', {'class': 'button'}).t(tab_bar_safe);
-e.i('div', {'class': 'button'}).t(tab_bar_safe);
+let trends = e.i('div', {'class': 'button trends'}).t(tab_bar_safe);
+e.i('text', {'class': 'text_tab_bar text_tab_bar_trends'}, {}, `Trends`).t(trends);
+let whatsup = e.i('div', {'class': 'button whatsup'}).t(tab_bar_safe);
+e.i('text', {'class': 'text_tab_bar text_tab_bar_whatsup'}, {}, `Whatsup`).t(whatsup);
